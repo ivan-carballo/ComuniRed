3614495cf6e1903aeec063d6c0484dc0a3cac3ec
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-07-19 10:17:31 +0200

    Boton en otherPerfil para poder enviar mensajes privados, aun sin guardar datos, solo icono y scss

--- front/src/componentes/otherPerfil/perfilData.jsx
+++ front/src/componentes/otherPerfil/perfilData.jsx
@@ -1,7 +1,13 @@
+// Componente para la cabecera de su perfil visto desde otro usuario
+// Contiene los datos basicos del usuario
+
+
 import React from "react";
 import { useState, useEffect } from "react";
 import { getUserByID } from "../../api/userAPI";
 import { getPostByProperty } from "../../api/postAPI";
+import { FaInbox } from 'react-icons/fa'
+
 
 import '../../saas/otherPerfil/perfilData.scss'
 
@@ -13,17 +19,20 @@ function PerfilData({id}) {
     const [post, setPost] = useState()
 
 
+    // UseEffect para recopilar los datos del usuario con una llamada API
     useEffect(() => {
         showUser(id)
         async function showUser(id) {
             let getOtherUser = await getUserByID(id)
             getOtherUser = getOtherUser.data
 
+            // Sacar solo el mes y año de la fecha de registro
             let date = getOtherUser.dateString
             date = date.split(",")[0].split(" de ")
             date = `${date[1]} ${date[2]}`
             date = date.charAt(0).toUpperCase() + date.slice(1).toLowerCase()
 
+            // Buscar todos los posts del usuario para la estadistica
             const getPostByUser = await getPostByProperty('userID', id)
 
             setUsername(getOtherUser.username)
@@ -42,7 +51,10 @@ function PerfilData({id}) {
 
                 <img src={perfilIMG} />
                 <div id="perfilData-p">
-                    <p id='perfilData-user'>{username}</p>
+                    <div id="perfilData-header">
+                        <p id='perfilData-user'>{username} </p>
+                        <FaInbox id='perfilData-inbox' />
+                    </div>
                     <p id='perfilData-date'>Registrado: {date}</p>
                     <p id='perfilData-post'>Nº post: {post}</p>
                 </div>
