333c96c198846b540da39e55e23c724b15770ce1
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-07-23 09:45:25 +0200

    Usando el nombre de usuario en las notificaciones de Follow, se puede ir al perfil y eliminar la notificacion

--- front/src/componentes/notification/notificationFollow.jsx
+++ front/src/componentes/notification/notificationFollow.jsx
@@ -13,8 +13,48 @@ function NotificationFollow() {
     const navigate = useNavigate()
     const userCurrentID = Cookies.get('id')
 
+    const [reboot, setReboot] = useState(true)
     const [data, setData] = useState()
 
+    
+
+    // useEffect para obtener todas las notificaciones del usuario logueado y mostrarlas en pantalla con un metodo map
+    useEffect(() => {
+        if(reboot) {
+
+            getNotiFollow()
+            async function getNotiFollow() {
+                const getNotiFollowByUser = await getNotiFollowByProperty('followerID', userCurrentID)
+
+                const notiFollowMap = getNotiFollowByUser.data.reverse().map((data) => 
+                    <div id="notiFollowMap-div" key={data._id}>
+                        <p id="notiFollowMap-date">{data.dateString}</p>
+                        <div id="notiFollowMap-data">
+                            <img id="notiFollowMap-data-img" src={data.img}></img>
+                            <p id="notiFollowMap-data-username" value={data.followID} onClick={checkFollow}>{data.username}</p>
+                            <input type="button" value='seguir' id="notiFollowMap-data-button" />
+                        </div>
+                    </div>
+                )
+                setData(notiFollowMap)
+            }
+        }
+    }, [reboot])
+
+
+
+    // Funcion para ir al perfil de un seguidor pulsando el nombre y eliminar la notificacion asociada a ese seguidor
+    async function checkFollow(e) {
+        const getFollowID = e.target.attributes[1].value
+        
+        // Buscar el ID de la notificacion y eliminarlo
+        const getFollowByFollowID = await getNotiFollowByProperty('followerID', userCurrentID)
+        const getFollowerFilter = await getFollowByFollowID.data.filter(data => data.followID == getFollowID)
+        const notiFollowRemove = await notiFollowDelete(getFollowerFilter[0]._id)
+
+        // Llevar al perfil del usuario
+        navigate(`/user/${getFollowID}`)
+    }
 
 
 
@@ -24,6 +64,7 @@ function NotificationFollow() {
         const followDeleteMap = Promise.all( getAllNotiFollow.data.map( async (data) => {
             const followAllDelete = await notiFollowDelete(data._id)
         }))
+        setReboot(true)
         
     }
 
