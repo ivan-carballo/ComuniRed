78b9749594e7a10154cad5374dae3b0d8ecd0c2b
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-07-25 20:41:31 +0200

    Limpieza de dependencias npm

--- back/config/emailService.js
+++ back/config/emailService.js
@@ -1,26 +1,6 @@
-import nodemailer from 'nodemailer'
 import dotenv from 'dotenv';
-import { Resend } from 'resend';
 
 dotenv.config();
 
 
 
-
-const resend = new Resend('re_123456789');
-
-(async function () {
-  const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
-    to: ['delivered@resend.dev'],
-    subject: 'Hello World',
-    html: '<strong>It works!</strong>',
-  });
-
-  
-  if (error) {
-    return console.error({ error });
-  }
-
-  console.log({ data });
-})();
