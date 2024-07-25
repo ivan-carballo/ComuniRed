ea36ddcc8433d80c572e9e5f0f4edd3e120de191
ivan-carballo <149379998+ivan-carballo@users.noreply.github.com>
2024-07-25 20:50:50 +0200

    Update emailService.js

--- back/config/emailService.js
+++ back/config/emailService.js
@@ -7,33 +7,34 @@ const apiKey = '4f9935f315b5d9cdb36645e5c31a0f7b'
 const secretKey = 'e18f8d62f5f50f6326d516590f0c45bb'
 
 
-.connect('****************************1234', '****************************abcd')
-const request = mailjet
-.post("send", {'version': 'v3.1'})
-.request({
-  "Messages":[
+const mailjet = require('node-mailjet').connect(
+  apiKey,
+  secretKey
+)
+const request = mailjet.post('send', { version: 'v3.1' }).request({
+  Messages: [
     {
-      "From": {
-        "Email": "79.umaru@gmail.com",
-        "Name": "Lacko"
+      From: {
+        Email: '79.umaru@gmail.com',
+        Name: 'Me',
       },
-      "To": [
+      To: [
         {
-          "Email": "79.umaru@gmail.com",
-          "Name": "Lacko"
-        }
+          Email: '79.umaru@gmail.com',
+          Name: 'You',
+        },
       ],
-      "Subject": "Greetings from Mailjet.",
-      "TextPart": "My first Mailjet email",
-      "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
-      "CustomID": "AppGettingStartedTest"
-    }
-  ]
+      Subject: 'My first Mailjet Email!',
+      TextPart: 'Greetings from Mailjet!',
+      HTMLPart:
+        '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
+    },
+  ],
 })
 request
-  .then((result) => {
+  .then(result => {
     console.log(result.body)
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err.statusCode)
   })
