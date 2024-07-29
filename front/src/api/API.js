7d4c136ac4b2e3c7273b49ba06b6bb17340573d2
Ubuntu <ubuntu@vps-52e07ef0.vps.ovh.net>
2024-07-29 10:13:25 +0000

    Cambios para servidor

--- front/src/api/API.js
+++ front/src/api/API.js
@@ -1,6 +1,6 @@
 // Ruta predeterminada del servidor local para evitar tener que modificar varios archivos si deseo cambiarla
 
-const API_URL = 'http://localhost:3015'
+const API_URL = 'http://api.comunired.ivancm.info'
 
 export {
     API_URL
