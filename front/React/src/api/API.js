b31448997a41e32a5e62812b43baba7d4d1a8ae2
Ubuntu <ubuntu@vps-52e07ef0.vps.ovh.net>
2024-07-29 16:46:56 +0000

    Limpieza de archivos innecesarios

--- front/React/src/api/API.js
+++
@@ -1,7 +0,0 @@
-// Ruta predeterminada del servidor local para evitar tener que modificar varios archivos si deseo cambiarla
-
-const API_URL = 'https://api.comunired.ivancm.info'
-
-export {
-    API_URL
-}
